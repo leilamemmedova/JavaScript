3f5b1356a46b4ebf2185c149f72c5b41ed877309
Leyla <127010691+leilamemmedova@users.noreply.github.com>
2023-05-10 11:14:39 -0700

    Add files via upload

--- ClassWork/20.MOCK_API/script.js
+++ ClassWork/20.MOCK_API/script.js
@@ -1,30 +1,46 @@
-let cards = document.querySelector(".cards");
-let card = document.querySelector(".card");
+let cards=document.querySelector(".cards")
+let card=document.querySelector(".card")
+let deleteBtn=document.querySelector(".delete")
+let editBtn=document.querySelector(".edit")
+let addBtn=document.querySelector(".addBtn")
 
-let BASE_URL = "https://northwind.vercel.app/api/categories";
+let BASE_URL ="https://northwind.vercel.app/api/categories"
+// axios(BASE_URL).then((response)=>console.log(response.data))
 
-function getAllUsers() {
-  axios(BASE_URL).then((response) => {
-    response.data.forEach((item) => {
-      cards.innerHTML += `
-                <div class="col col-6">
-              <div class="card d-flex justify-content-between align-items-center m-3 border-2">
-                <div class="left">
-                    <div class="row">
-                        <span>${item.name}</span>
-                    </div>
-                    <div class="row">
-                        <span>${item.description}</span>
-                    </div>
+function getAllUsers(){
+    axios(BASE_URL)
+      .then((response) => 
+      response.data.forEach((item) => {
+        cards.innerHTML += `
+            <div class="col col-6">
+          <div class="card d-flex justify-content-between align-items-center m-3">
+            <div class="left">
+                <div class="row">
+                    <span>${item.name}</span>
                 </div>
-                <div class="right">
-                    <i class="fa-solid fa-pencil" style="color: #129900;"></i>
-                    <i class="fa-solid fa-trash-can" style="color: #e60000;"></i>
+                <div class="row">
+                    <span>${item.description}</span>
                 </div>
-              </div>
             </div>
-            `;
-    });
-  });
+            <div class="right">
+                <i class="fa-solid fa-pencil edit" style="color: #129900;"></i>
+                <i class="fa-solid fa-trash-can delete" style="color: #e60000;" onclick=deleteUser(${item.id}, this)></i>
+            </div>
+          </div>
+        </div>
+        `;
+      })
+)}
+getAllUsers()
+
+function deleteUser(id,btn){
+    axios.delete(`BASE_URL/${id}`)
+    console.log(id)
+    btn.parentElement.parentElement.remove()
+    getAllUsers()
 }
-getAllUsers();
+deleteUser()
+
+addBtn.addEventListener("click", function(){
+    window.location = "http://127.0.0.1:5500/addUsers.html";
+})
