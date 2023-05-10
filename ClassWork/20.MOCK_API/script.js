2744666ca4665a976c9a907525b7b87e7906a5a6
Leyla <127010691+leilamemmedova@users.noreply.github.com>
2023-05-09 23:21:43 -0700

    Add files via upload

--- ClassWork/20.MOCK_API/script.js
+++ ClassWork/20.MOCK_API/script.js
@@ -1,29 +1,30 @@
-let cards=document.querySelector(".cards")
-let card=document.querySelector(".card")
+let cards = document.querySelector(".cards");
+let card = document.querySelector(".card");
 
-let BASE_URL ="https://northwind.vercel.app/api/categories"
-axios(BASE_URL).then((response)=>console.log(response.data))
+let BASE_URL = "https://northwind.vercel.app/api/categories";
 
-function getAllUsers(){
-    BASE_URL.forEach(item => {
-        cards.innerHTML +=`
-            <div class="col col-6">
-          <div class="card d-flex justify-content-between align-items-center m-3">
-            <div class="left">
-                <div class="row">
-                    <span>${item.name}</span>
+function getAllUsers() {
+  axios(BASE_URL).then((response) => {
+    response.data.forEach((item) => {
+      cards.innerHTML += `
+                <div class="col col-6">
+              <div class="card d-flex justify-content-between align-items-center m-3 border-2">
+                <div class="left">
+                    <div class="row">
+                        <span>${item.name}</span>
+                    </div>
+                    <div class="row">
+                        <span>${item.description}</span>
+                    </div>
                 </div>
-                <div class="row">
-                    <span>${item.description}</span>
+                <div class="right">
+                    <i class="fa-solid fa-pencil" style="color: #129900;"></i>
+                    <i class="fa-solid fa-trash-can" style="color: #e60000;"></i>
                 </div>
+              </div>
             </div>
-            <div class="right">
-                <i class="fa-solid fa-pencil" style="color: #129900;"></i>
-                <i class="fa-solid fa-trash-can" style="color: #e60000;"></i>
-            </div>
-          </div>
-        </div>
-        `
+            `;
     });
+  });
 }
-getAllUsers()
+getAllUsers();
